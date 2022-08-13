4976b0a2d49316d2270fcd8cb2aece94110bffb4
TrungKiennef <109646424+TrungKiennef@users.noreply.github.com>
2022-08-13 20:52:14 +0700

    Create 7.3.cpp

---
+++ 7.3.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define Max 100
+
+void nhap (float a[], int &n)
+{
+	do
+{
+	printf("nhap so phan tu: ");
+	scanf("%d",&n);
+	if(n<=0 || n>Max)
+	{
+		printf("\nso phan tu ko hop le");
+}
+}while (n<=0||n>Max);
+for(int i=0;i<n;i++)
+{
+	printf("\nnhap a[%d]: ",i);
+	scanf("%f", &a[i]);
+}
+	
